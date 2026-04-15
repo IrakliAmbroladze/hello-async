bd6f92568f3655622c312a261125da6bb27781ee
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-15 10:24:15 +0400

    refactor: async function of page_title using futer return type

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use std::future::Future;
 use trpl::Html;
 
 fn main() {
     println!("Hello, world!");
 }
 
-async fn page_title(url: &str) -> Option<String> {
-    let response_text = trpl::get(url).await.text().await;
-    Html::parse(&response_text)
-        .select_first("title")
-        .map(|title_element| title_element.inner_html())
+fn page_title(url: &str) -> impl Future<Output = Option<String>> {
+    async move {
+        let text = trpl::get(url).await.text().await;
+        Html::parse(&text)
+            .select_first("title")
+            .map(|title_element| title_element.inner_html())
+    }
 }
