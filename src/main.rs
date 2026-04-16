3a0f5ff63dd8df1b352ae4148162d9fdd19715d5
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-16 10:20:47 +0400

    feat: implement Awaiting an async block with trpl::run

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ use trpl::Html;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     println!("{:#?}", args);
+    trpl::run(async {
+        let url = &args[1];
+        match page_title(url).await {
+            Some(title) => println!("The title for {url} was {title}"),
+            None => println!("{url} had no title"),
+        }
+    });
 }
 
 fn page_title(url: &str) -> impl Future<Output = Option<String>> {
