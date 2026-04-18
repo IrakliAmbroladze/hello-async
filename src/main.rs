2168134006d3d3f7ab9e5efe82584def89807833
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-18 08:21:37 +0400

    use Duration in async block

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,8 @@
-use trpl::{Either, Html};
-
+use std::time::Duration;
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    println!("{:#?}", args);
     trpl::run(async {
-        let title_fut_1 = page_title(&args[1]);
-        let title_fut_2 = page_title(&args[2]);
-
-        let (url, maybe_title) = match trpl::race(title_fut_1, title_fut_2).await {
-            Either::Left(left) => left,
-            Either::Right(right) => right,
-        };
-
-        println!("{url} returned first");
-        match maybe_title {
-            Some(title) => println!("Its page title is: '{title}'"),
-            None => println!("Its title could not be parsed."),
-        }
+        println!("First sentence");
+        trpl::sleep(Duration::from_millis(3000)).await;
+        println!("Second sentence after 3 seconds");
     });
-}
-
-async fn page_title(url: &str) -> (&str, Option<String>) {
-    let text = trpl::get(url).await.text().await;
-    let title = Html::parse(&text)
-        .select_first("title")
-        .map(|title_element| title_element.inner_html());
-    (url, title)
 }
