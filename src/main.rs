e6e29e8ee46e576f1cdb1548ac70bc196e3041fb
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-20 11:32:30 +0400

    feat: use different Output types in join macro

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,10 @@
-use std::pin::{pin, Pin};
-use std::time::Duration;
-
 fn main() {
     trpl::run(async {
-        let (tx, mut rx) = trpl::channel();
+        let a = async { 1u32 };
+        let b = async { "Hello!" };
+        let c = async { true };
 
-        let tx1 = tx.clone();
-        let tx1_fut = pin!(async move {
-            let vals = vec![
-                String::from("hi"),
-                String::from("from"),
-                String::from("the"),
-                String::from("future"),
-            ];
-
-            for val in vals {
-                tx1.send(val).unwrap();
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        });
-        let rx_fut = pin!(async {
-            while let Some(value) = rx.recv().await {
-                println!("received '{value}'");
-            }
-        });
-        let tx_fut = pin!(async move {
-            let vals = vec![
-                String::from("more"),
-                String::from("messages"),
-                String::from("for"),
-                String::from("you"),
-            ];
-
-            for val in vals {
-                tx.send(val).unwrap();
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        });
-        let futures: Vec<Pin<&mut dyn Future<Output = ()>>> = vec![tx1_fut, rx_fut, tx_fut];
-        trpl::join_all(futures).await;
-    });
+        let (a_result, b_result, c_result) = trpl::join!(a, b, c);
+        println!("{a_result}, {b_result}, {c_result}")
+    })
 }
