b0b236bc9e0edb37b75c7dd2c2f238f05c6cc552
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-21 17:09:20 +0400

    Using yield_now to let operations switch off makong progress

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
 use std::{thread, time::Duration};
 fn main() {
     trpl::run(async {
-        let one_ms = Duration::from_millis(1);
         let a = async {
             println!("'a' started.");
             slow("a", 30);
-            trpl::sleep(one_ms).await;
+            trpl::yield_now().await;
             slow("a", 10);
-            trpl::sleep(one_ms).await;
+            trpl::yield_now().await;
             slow("a", 20);
-            trpl::sleep(one_ms).await;
+            trpl::yield_now().await;
             println!("'a' finished.");
         };
 
         let b = async {
             println!("'b' started.");
             slow("b", 75);
-            trpl::sleep(one_ms).await;
+            trpl::yield_now().await;
             slow("b", 10);
-            trpl::sleep(one_ms).await;
+            trpl::yield_now().await;
             slow("b", 15);
-            trpl::sleep(one_ms).await;
+            trpl::yield_now().await;
             slow("b", 350);
-            trpl::sleep(one_ms).await;
+            trpl::yield_now().await;
             println!("'b' finished.");
         };
 
