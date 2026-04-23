e7a99c2d30c86e97ed70aba791c14992c2b149f6
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-23 10:10:20 +0400

    Using the rx receiver as a ReceiverStream

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use trpl::StreamExt;
+use trpl::{ReceiverStream, Stream, StreamExt};
 
 fn main() {
     trpl::run(async {
-        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let iter = values.iter().map(|n| n * 2);
-        let stream = trpl::stream_from_iter(iter);
-
-        let mut filtered = stream.filter(|value| value % 3 == 0 || value % 5 == 0);
-        while let Some(value) = filtered.next().await {
-            println!("The value is: {value}");
+        let mut messages = get_messages();
+        while let Some(message) = messages.next().await {
+            println!("{message}");
         }
-    })
+    });
+}
+
+fn get_messages() -> impl Stream<Item = String> {
+    let (tx, rx) = trpl::channel();
+    let messages = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+    for message in messages {
+        tx.send(format!("Message: '{message}'")).unwrap();
+    }
+    ReceiverStream::new(rx)
 }
