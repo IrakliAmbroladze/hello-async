03833f09d6ddaf68a2f16be8bdbeb36d8113bad3
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-04-25 14:49:54 +0400

    Listing 17-40: Handling errors and shutting down the loops

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,15 @@ fn main() {
     trpl::run(async {
         let messages = get_messages().timeout(Duration::from_millis(200));
         let intervals = get_intervals()
-            .map(|count| format!("Interval: {count}"))
-            .throttle(Duration::from_millis(100))
+            .map(|count| format!("Interval #{count}"))
+            .throttle(Duration::from_millis(500))
             .timeout(Duration::from_secs(10));
         let merged = messages.merge(intervals).take(20);
         let mut stream = pin!(merged);
+
         while let Some(result) = stream.next().await {
             match result {
-                Ok(message) => println!("Message is: {message}"),
+                Ok(item) => println!("{item}"),
                 Err(reason) => eprintln!("Problem: {reason:?}"),
             }
         }
@@ -22,12 +23,17 @@ fn main() {
 
 fn get_messages() -> impl Stream<Item = String> {
     let (tx, rx) = trpl::channel();
+
     trpl::spawn_task(async move {
         let messages = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+
         for (index, message) in messages.into_iter().enumerate() {
             let time_to_sleep = if index % 2 == 0 { 100 } else { 300 };
             trpl::sleep(Duration::from_millis(time_to_sleep)).await;
-            tx.send(format!("Message: '{message}'")).unwrap();
+
+            if let Err(send_error) = tx.send(format!("Message: '{message}'")) {
+                eprintln!("Cannot send message '{message}': {send_error}");
+            }
         }
     });
     ReceiverStream::new(rx)
@@ -35,12 +41,16 @@ fn get_messages() -> impl Stream<Item = String> {
 
 fn get_intervals() -> impl Stream<Item = i32> {
     let (tx, rx) = trpl::channel();
+
     trpl::spawn_task(async move {
         let mut count = 0;
         loop {
             trpl::sleep(Duration::from_millis(1)).await;
             count += 1;
-            tx.send(count).unwrap();
+
+            if let Err(send_error) = tx.send(count) {
+                eprintln!("Cannot send interval '{count}': {send_error}")
+            };
         }
     });
     ReceiverStream::new(rx)
